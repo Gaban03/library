1b2da1ccec29a0cd185018210c551097d418f299
NicolasRibe <oloconicao@gmail.com>
2024-12-04 21:46:37 -0300

    add booKrepository

--- project_java_upgrade/src/repository/BookRepository.java
+++ project_java_upgrade/src/repository/BookRepository.java
@@ -1,5 +1,39 @@
 package repository;
 
-public class BookRepository {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.text.ParseException;
 
+import db.DB;
+
+public class BookRepository {
+	
+	
+	public static  void inserrirLivro(String titulo, String autor, String isbn) {
+		PreparedStatement st = null;
+		Connection conn= null;
+		
+		try{
+			
+			Connection conn = DB.getConnection();
+			
+			st = conn.prepareStatement(				
+					"INSERT INTO book "
+					+ "(titulo, autor, isbn) "
+					+ "VALUES"
+					+ "(?, ?, ?)");
+					
+					
+					st.setString(1, titulo);
+					st.setString(2, autor);
+					st.setString(2, isbn);
+		}
+		catch(ParseException e) {
+				e.printStackTrace();
+		}
+		finally {
+			DB.closeConnection();
+			DB.closeStatement(st);
+		}
+	}
 }
