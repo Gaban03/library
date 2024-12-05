01b13d1728833d2fea470d1d9c3d1ffb9c90db5b
Murilo-Herrick <muriloherrick@gmail.com>
2024-12-04 21:29:55 -0300

    UserRepository pronto!

--- project_java_upgrade/src/repository/UserRepository.java
+++ project_java_upgrade/src/repository/UserRepository.java
@@ -1,6 +1,42 @@
 package repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import db.DB;
+
 public class UserRepository {
-	
-	
+
+	public static void UserInsert(String nome) {
+
+		Connection conn = null;
+		PreparedStatement st = null;
+
+		try {
+			conn = DB.getConnection();
+
+			st = conn.prepareStatement("INSERT INTO user (nome) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, nome);
+
+			int registros = st.executeUpdate();
+
+			if (registros > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Finalizado! Tabela usuario atualizada! ID = " + id);
+				}
+			} else {
+				System.out.println("Nenhum registro na tabela de usuario alterado!");
+			}
+
+		} catch (SQLException e) {
+
+		}
+	}
+
 }
